706dc2ced7c62e02d3d9312652d0239c37eec14a
camille-s <camille.seaberry@gmail.com>
2023-06-22 21:28:46 -0400

    Clean up stray vignette materials

--- tests/testthat/test-round_sum100.R
+++ tests/testthat/test-round_sum100.R
@@ -5,13 +5,10 @@ test_that("round_sum100 rounds correctly", {
 })
 
 test_that("round_sum100 prints messages", {
-  expect_message(round_sum100(c(0.24, 0.61, 0.15) * 100, 0, verbose = TRUE),
-                 ":-)", fixed = TRUE)
-  expect_message(round_sum100(c(0.24, 0.6, 0.15) * 100, verbose = TRUE),
-                 ":-(", fixed = TRUE)
+  expect_message(round_sum100(c(0.24, 0.6, 0.15) * 100, verbose = TRUE), "Sums to 99, not 100.")
   expect_silent(s2 <- sum(round_sum100(c(9.2124, 40.292, 50.2), 0, verbose = FALSE)))
 })
 
-test_that("round_sum100 fails if any NA values", {
-  expect_error(round_sum100(c(0.24, 0.61, NA) * 100))
+test_that("round_sum100 warns if any NA values", {
+  expect_warning(round_sum100(c(0.24, 0.61, NA) * 100))
 })
